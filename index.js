4ad4caaf2f3cb27e94ee08e1eb5fb44d48e8a15c
Eslam M. Tammam <eslamtammam@hotmail.com>
2022-03-05 12:11:49 +0200

    adding league information to each match object including league name and league logo url. also change the logo to logoUrl

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Port = process.env.PORT || 3000;
 const baseUrl = 'https://www.yallakora.com/Match-Center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A%D8%A7%D8%AA';
 
 
-
 const app = express();
 const matches = [];
 
@@ -16,56 +15,65 @@ async function getMatches(url) {
         .then(response => {
             const html = response.data;
             const content = cheerio.load(html);
-            content('.matchObj', html).each(function () {
+            content('.matchItem', html).each(function () {
+
                 const teamAGoalsArray = [];
                 const teamBGoalsArray = [];
-                const teamAName = content(this).find('div.teamA').find('a.teamName').text().trim();
-                const teamAScore = content(this).find('div.resultDiv').find('div.matchResult').find('span.LiveTeamA').text();
-                content(this).find('div.teamA').find('div.matchScorer').find('div.goal').each(function (i, el) {
-                    const player = content(el).find('a.player').text().trim();
-                    const time = content(el).find('span.time').text().trim();
-                    teamAGoalsArray.push({
-                        Index: i,
-                        player: player,
-                        time: time
-                    })
-                });
-                const teamALogoUrl = content(this).find('div.teamA').find('a').find('img').attr('src');
 
-                const teamBName = content(this).find('div.teamB').find('a.teamName').text().trim();
-                const teamBScore = content(this).find('div.resultDiv').find('div.matchResult').find('span.LiveTeamB').text();
-                content(this).find('div.teamB').find('div.matchScorer').find('div.goal').each(function (i, el) {
-                    const player = content(el).find('a.player').text().trim();
-                    const time = content(el).find('span.time').text().trim();
-                    teamBGoalsArray.push({
-                        Index: i,
-                        player: player,
-                        time: time
-                    })
-                });
-                const teamBLogoUrl = content(this).find('div.teamB').find('a').find('img').attr('src');
+                const leagueName = content(this).find('div.ttl').find('a').find('img').attr('alt');
+                const leagueLogo = content(this).find('div.ttl').find('a').find('img').attr('src');
 
-                const matchTime = content(this).find('div.resultDiv').find('div.matchTime').text().trim();
-                const week = content(this).find('div.week').text().trim();
-                const tvChannel = content(this).find('div.icon-tv').text().trim();
-                matches.push({
-                    host: {
-                        name: teamAName,
-                        logo: teamALogoUrl,
-                        score: treatNoneScore(teamAScore),
-                        goals: teamAGoalsArray
-                    },
-                    visitor: {
-                        name: teamBName,
-                        logo: teamBLogoUrl,
-                        score: treatNoneScore(teamBScore),
-                        goals: teamBGoalsArray
-                    },
-                    matchTime: matchTime,
-                    week: week,
-                    tvChannel: tvChannel
-                });
+                content(this).find('ul.mtchObjContainer').find('.matchObj')
+                    .each(function (i, league) {
+                        const teamAName = content(league).find('div.teamA').find('a.teamName').text().trim();
+                        const teamAScore = content(league).find('div.resultDiv').find('div.matchResult').find('span.LiveTeamA').text();
+                        content(league).find('div.teamA').find('div.matchScorer').find('div.goal').each(function (i, el) {
+                            const player = content(el).find('a.player').text().trim();
+                            const time = content(el).find('span.time').text().trim();
+                            teamAGoalsArray.push({
+                                player: player,
+                                time: time
+                            })
+                        });
+                        const teamALogoUrl = content(league).find('div.teamA').find('a').find('img').attr('src');
 
+                        const teamBName = content(league).find('div.teamB').find('a.teamName').text().trim();
+                        const teamBScore = content(league).find('div.resultDiv').find('div.matchResult').find('span.LiveTeamB').text();
+                        content(league).find('div.teamB').find('div.matchScorer').find('div.goal').each(function (i, el) {
+                            const player = content(el).find('a.player').text().trim();
+                            const time = content(el).find('span.time').text().trim();
+                            teamBGoalsArray.push({
+                                player: player,
+                                time: time
+                            })
+                        });
+                        const teamBLogoUrl = content(league).find('div.teamB').find('a').find('img').attr('src');
+
+                        const matchTime = content(league).find('div.resultDiv').find('div.matchTime').text().trim();
+                        const week = content(league).find('div.week').text().trim();
+                        const tvChannel = content(league).find('div.icon-tv').text().trim();
+                        matches.push({
+                            league: {
+                                name: leagueName,
+                                logoUrl: leagueLogo
+                            },
+                            host: {
+                                name: teamAName,
+                                logoUrl: teamALogoUrl,
+                                score: treatNoneScore(teamAScore),
+                                goals: teamAGoalsArray
+                            },
+                            visitor: {
+                                name: teamBName,
+                                logoUrl: teamBLogoUrl,
+                                score: treatNoneScore(teamBScore),
+                                goals: teamBGoalsArray
+                            },
+                            matchTime: matchTime,
+                            week: week,
+                            tvChannel: tvChannel
+                        });
+                    });
             });
         });
 }
@@ -74,12 +82,12 @@ function healthCheck() {
     return 'hello Mario, your princess is in another castle';
 }
 
-function isGoodDate(dt){
+function isGoodDate(dt) {
     const reGoodDate = /^(0[1-9]|1[012])[\/.](0[1-9]|[12][0-9]|3[01])[\/.](19|20)[0-9]{2}$/;
     return reGoodDate.test(dt);
 }
 
-function treatNoneScore(scores){
+function treatNoneScore(scores) {
     if (scores === "-")
         return 0;
 }
